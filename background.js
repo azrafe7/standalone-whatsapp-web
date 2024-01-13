b0f420927098f17a230a6cf47637ea125e6f9913
Giuseppe Di Mauro <azrafe7@gmail.com>
2024-01-13 15:32:28 +0100

    [v0.1.2] only open one popup, contextmenu entry to reattach

--- background.js
+++ background.js
@@ -3,12 +3,39 @@
 let manifest = chrome.runtime.getManifest();
 console.log(manifest.name + " v" + manifest.version);
 
-const url = 'https://web.whatsapp.com';
+// const url = 'https://web.whatsapp.com';
+const url = 'https://www.wikipedia.org/';
 
 let size = {
   width: 800,
   height: 600,
 };
+
+let createdPopupWin = null;
+
+// add/remove contextMenu entry to action button
+const reattachContextId = "StandaloneWA_onReattachContextMenu";
+function updateContextMenu(options={}) {
+  const defaults = { enable:true };
+  options = { ...defaults, ...options };
+  chrome.contextMenus.removeAll(function() {
+    // console.log("remove");
+    if (chrome.runtime.lastError) {
+      console.warn('Whoops...', chrome.runtime.lastError.message);
+    } else if (options.enable) {
+      chrome.contextMenus.create({
+        id: reattachContextId,
+        title: "Re-attach as tab...",
+        contexts: ["action"],
+      }, () => {
+        // console.log("create");
+        if (chrome.runtime.lastError) {
+          console.warn('Whoops...', chrome.runtime.lastError.message);
+        }      
+      });
+    }
+  });
+}
 
 chrome.action.onClicked.addListener(async (tab) => {
   console.log("[StandaloneWA:BG] clicked");
@@ -20,5 +47,41 @@ chrome.action.onClicked.addListener(async (tab) => {
     height: size.height,
   };
   
-  chrome.windows.create(options);
+  let currWindow = await chrome.windows.getCurrent();
+  
+  if (!createdPopupWin) { // create new popup
+    chrome.windows.create(options, (win) => {
+      createdPopupWin = win;
+      console.log('created', createdPopupWin);
+    });
+    
+    console.log('currWindow:', currWindow);
+  } else {  // focus existing popup
+    chrome.windows.update(createdPopupWin.id, { focused:true });
+  }
+  
+  updateContextMenu({enable:true});
+});
+
+chrome.windows.onRemoved.addListener((winId) => {
+  console.log("removed win", winId);
+  if (createdPopupWin && winId == createdPopupWin.id) {
+    createdPopupWin = null;
+    updateContextMenu({enable:false});
+  }
+});
+
+chrome.contextMenus.onClicked.addListener(async (info, tab) => {
+  console.log("[StandaloneWA:BG] onContextMenuClicked:", [info, tab]);
+
+  if (info.menuItemId === reattachContextId) {
+    console.log("[StandaloneWA:BG] reattach as tab...");
+    
+    let currWindow = await chrome.windows.getCurrent();
+    chrome.tabs.move(createdPopupWin.tabs[0].id, { windowId:currWindow.id, index:-1 }, (tab) => {
+      chrome.tabs.update(tab.id, { active:true });
+      createdPopupWin = null;
+      updateContextMenu({enable:false});
+    });
+  }
 });
